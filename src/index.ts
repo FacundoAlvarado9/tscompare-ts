f7818833cc28b8f235578f34928414ae2fb7afa5
Facundo Alvarado <facundoalvarado9@gmail.com>
2025-07-06 19:23:06 -0300

    Dedicated Weighted/Unweighted Euclidean distance classes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './types/TSComparator.types';
 export * from './TSComparator';
 export * from './strategies/IDistanceStrategy';
-export * from './strategies/EuclideanDistance';
+export * from './strategies/UnweightedEuclidean';
+export * from './strategies/WeightedEuclidean';
 export * from './strategies/ManhattanDistance';
--- src/strategies/UnweightedEuclidean.ts
+++ src/strategies/UnweightedEuclidean.ts
@@ -1,7 +1,7 @@
-import type { NDimensionalPoint } from'../types/TSComparator.types';
+import type { NDimensionalPoint } from '../types/TSComparator.types';
 import { AbstractDistanceStrategy } from './AbstractDistanceStrategy';
 
-export class ManhattanDistance extends AbstractDistanceStrategy {
+export class UnweightedEuclidean extends AbstractDistanceStrategy{
 
     constructor(){
         super();
@@ -9,11 +9,11 @@ export class ManhattanDistance extends AbstractDistanceStrategy {
 
     protected calculateDistance(point1: NDimensionalPoint, point2: NDimensionalPoint): number {
         const variable_count = point1.length;
-        let sum_diffs = 0;
+        let sum_squared_diffs = 0;
         for(let i=0; i<variable_count; i++){
-            sum_diffs += Math.abs(point1[i]-point2[i]);
+            const dif = point1[i] - point2[i];
+            sum_squared_diffs += Math.pow(dif, 2);
         }
-        return sum_diffs;
+        return Math.sqrt(sum_squared_diffs);
     }
-
 }
